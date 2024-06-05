170d2491682ae72406dae1c2102aa2c0bfdf73cc
wenhwang97 <wenhwang@bu.edu>
2024-06-05 13:59:45 -0400

    fix circular import

--- server/app-sm-context/app/__init__.py
+++ server/app-sm-context/app/__init__.py
@@ -1,16 +1,16 @@
 import os
 
 from flask import Flask
-from app.main import main as main_blueprint
-from app.services.ollama import Ollama
 
 def create_app():
     app = Flask(__name__)
-    
+
+    from app.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from app.services.ollama import Ollama
     global ollama
     ollama = Ollama()
     ollama.load_google_documents(id=os.getenv('GOOGLE_DRIVE_FOLDER_ID'))
-
-    app.register_blueprint(main_blueprint)
 
     return app
