f80525e358e5d420867a95abafd5774dac4015b1
wenhwang97 <wenhwang@bu.edu>
2024-06-20 20:19:41 -0400

    fix batch size

--- google_drive/llm.py
+++ google_drive/llm.py
@@ -48,6 +48,11 @@ class Ollama:
             self.db = Chroma(persist_directory=CHROMA_PATH, embedding_function=GPT4AllEmbeddings())
             self.retriever = self.db.as_retriever(search_kwargs={'k': 4})
             self.qa_chain = create_stuff_documents_chain(self.ollama, qa_prompt)
+        else:
+            print('Initializing Chroma...')
+            self.db = Chroma(embedding_function=GPT4AllEmbeddings(), persist_directory=CHROMA_PATH)
+            self.retriever = self.db.as_retriever(search_kwargs={'k': 4})
+            self.qa_chain = create_stuff_documents_chain(self.ollama, qa_prompt)
         timeend = time.time()
 
         print('Time taken to initialize Chroma: ', timeend - timestart, 'seconds')
@@ -58,13 +63,19 @@ class Ollama:
 
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
         chunks = text_splitter.split_documents(data)
+        max_batch_size = 5461
 
-        if not os.path.exists(CHROMA_PATH):
-            self.db = Chroma.from_documents(chunks, GPT4AllEmbeddings(), persist_directory=CHROMA_PATH)
-            self.retriever = self.db.as_retriever(search_kwargs={'k': 4})
-            self.qa_chain = create_stuff_documents_chain(self.ollama, qa_prompt)
-        else:
-            self.db.add_documents(chunks)
+        for i in range(0, len(chunks), max_batch_size):
+            batch = chunks[i:i + max_batch_size]
+            self.db.add_documents(batch)
+            print(f'Batch {i // max_batch_size + 1} added, size: {len(batch)}')
+            
+        # if not os.path.exists(CHROMA_PATH):
+        #     self.db = Chroma.from_documents(chunks, GPT4AllEmbeddings(), persist_directory=CHROMA_PATH)
+        #     self.retriever = self.db.as_retriever(search_kwargs={'k': 4})
+        #     self.qa_chain = create_stuff_documents_chain(self.ollama, qa_prompt)
+        # else:
+        #     self.db.add_documents(chunks)
 
     def load_google_drive(self, id, chunk_size=500, chunk_overlap=10):
         timestart = time.time()
